cf8e1025c90daffc698501aad5dd53a7e3c08a24
IainFullelove <fullelove.iain@gmail.com>
2020-08-03 10:03:23 +1000

    updated formatting of function parameters in vulcan core

--- mining_utils/vulcan/core.py
+++ mining_utils/vulcan/core.py
@@ -1,8 +1,8 @@
-'''
+"""
 This module is used to directly integrate with Maptek Vulcan by using its default executables.
 For the functions to run correctly, you must have a valid license installed and the dongle attached.
 This code has been tested using Maptek Vulcan 10.1 & Vulcan 8.1.
-'''
+"""
 
 import sys
 import os
@@ -11,19 +11,20 @@ import pandas as pd
 
 
 def vulcan_dir_to_path(parent_path):
-    '''
+    """
     adds Vulcan installation directory to path so that the correct executables can be found.
-    '''
+    """
     full_path = parent_path + '/bin/exe'
     sys.path.append("%r" % full_path)
     os.environ['VULCAN'] = full_path
     return
 
 
-def bdf_from_bmf(input_bmf_path, output_bdf_path):
-    '''
+def bdf_from_bmf(input_bmf_path,
+                 output_bdf_path):
+    """
     Use this to create a new block model definition file (.bdf) from an existing block model.
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/breverse'
     args = [exe, input_bmf_path, output_bdf_path]
@@ -32,22 +33,26 @@ def bdf_from_bmf(input_bmf_path, output_bdf_path):
 
 
 def create_bdf(output_bdf_path):
-    '''
+    """
     Create a basic regular Vulcan bdf that defines a block model framework.
     Used in conjunction with the csv_to_bmf / pandas_to_bmf functions when creating a bmf file.
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/bmine'
 
     return
 
 
-def bmf_to_csv(input_bmf_path, output_csv_path, bounding_triangulation=None, mask_variable=[None, None], test_condition=None):
-    '''
+def bmf_to_csv(input_bmf_path,
+               output_csv_path,
+               bounding_triangulation=None,
+               mask_variable=[None, None],
+               test_condition=None):
+    """
     Use this to export block values to a nominated CSV file. 
     The resulting CSV file can be edited through software packages that are familiar with the CSV format, such as Microsoft Excel and Microsoft Access. 
     The resulting file will be named after the original block model and stored in the same directory.
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/btocsv'
 
@@ -69,9 +74,9 @@ def bmf_to_csv(input_bmf_path, output_csv_path, bounding_triangulation=None, mas
 
 
 def csv_to_bmf(input_bmf_path):
-    '''
+    """
 
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/bmine'
 
@@ -79,19 +84,23 @@ def csv_to_bmf(input_bmf_path):
 
 
 def pandas_to_bmf(input_bmf_path):
-    '''
+    """
 
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/bmine'
 
     return
 
 
-def bmf_to_pandas(input_bmf_path, remove_header_rows=True, bounding_triangulation=None, mask_variable=[None, None], test_condition=None):
-    '''
+def bmf_to_pandas(input_bmf_path,
+                  remove_header_rows=True,
+                  bounding_triangulation=None,
+                  mask_variable=[None, None],
+                  test_condition=None):
+    """
     Read a regular block model file directly to a pandas dataframe in a single function
-    '''
+    """
     bmf_to_csv(input_bmf_path, 'temp.csv', bounding_triangulation=bounding_triangulation,
                mask_variable=mask_variable, test_condition=test_condition)
 
@@ -106,20 +115,22 @@ def bmf_to_pandas(input_bmf_path, remove_header_rows=True, bounding_triangulatio
 
 
 def mine_block_model(input_bmf_path):
-    '''
+    """
     Use this to report on the proportion of a block that falls in a nominated triangulation(s). 
     The 'mined-out' value, which can be reported as a percentage or fraction, will be written to a specified block model variable.
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/bmine'
 
     return
 
 
-def set_variable(input_bmf_path, variable, value):
-    '''
+def set_variable(input_bmf_path,
+                 variable,
+                 value):
+    """
     Use this to set a numeric block model variable. i.e. for resetting to 0 before flagging, etc...
-    '''
+    """
 
     value = float(value)
     variable = str(variable)
@@ -135,11 +146,13 @@ def set_variable(input_bmf_path, variable, value):
     return
 
 
-def create_variable(input_bmf_path, variable, value):
-    '''
+def create_variable(input_bmf_path,
+                    variable,
+                    value):
+    """
     Use this to create a numeric block model variable.
     Data type may be "float" or "int".
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/DGD2DWG'
 
@@ -147,9 +160,9 @@ def create_variable(input_bmf_path, variable, value):
 
 
 def strings_to_dxf():
-    '''
+    """
     Use this to export the design data contained in a Vulcan design database into a nominated drawing file (.dwg and .dxf).
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/DGD2DWG'
 
@@ -157,9 +170,9 @@ def strings_to_dxf():
 
 
 def dxf_to_strings():
-    '''
+    """
     Use this to export the design data contained in a Vulcan design database into a nominated drawing file (.dwg and .dxf).
-    '''
+    """
 
     exe = os.environ['VULCAN'] + '/DWG2DGD'
 
