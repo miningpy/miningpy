88b38c4032b4421c45c3c16a75bb1bad79b3d324
IainFullelove <fullelove.iain@gmail.com>
2020-08-08 14:26:02 +1000

    #13 ensure conda deployment works for python >=3.6

--- conda.recipe/conda-build.bat
+++ conda.recipe/conda-build.bat
@@ -6,6 +6,7 @@ conda-build conda.recipe --check
 
 REM build the recipe
 conda-build conda.recipe --no-anaconda-upload --channel conda-forge
+conda-build conda.recipe.nightly --no-anaconda-upload --channel conda-forge
 
 REM test the package
 conda-build conda.recipe --test
