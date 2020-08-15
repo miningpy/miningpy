31c637660937ed783462599b5055dc76d6662666
IainFullelove <fullelove.iain@gmail.com>
2020-08-15 22:14:34 +1000

    #27 working on plotting design strings

--- examples/dxf_plot02.py
+++ examples/dxf_plot02.py
@@ -1,16 +1,13 @@
-import ezdxf
-import pandas as pd
-import numpy as np
-import pyvista as pv
 import json
 import requests
 import hashlib
 import os
+import miningpy
 
 with open("examples/data_links.json", 'r') as file:
     data = json.load(file)
 
-url = data['dxf']['wireframe_vulcan']
+url = data['dxf']['strings_vulcan']
 
 file_id = url.split('/')[-2]
 dwn_url = 'https://drive.google.com/uc?export=download&id=' + file_id
@@ -23,48 +20,9 @@ temp_file = 'examples/' + hashlib.md5(dxf_raw.encode(encoding='UTF-8')).hexdiges
 with open(temp_file, 'w') as file:
     file.write(dxf_raw)
 
-# read temporary dxf file into ezdxf object
-doc = ezdxf.readfile(temp_file)
+# read temporary dxf file and plot using MiningPy function
+miningpy.plot3D_dxf(temp_file)
 
 # delete temporary dxf file that was downloaded
 os.remove(temp_file)
 
-msp = doc.modelspace()
-
-faces = msp.query('3DFACE')
-lines = msp.query('LINE')
-points = msp.query('POINT')
-
-# extract points and faces from dxf object
-num3Dfaces = len(faces)
-pointArray = np.zeros((num3Dfaces*3, 3))
-cellArray = np.zeros((num3Dfaces, 4), dtype='int')
-cellData = []
-
-i = 0
-faceCounter = 0
-name = []
-for e in faces:
-    pointArray[i, 0] = e.dxf.vtx0[0]
-    pointArray[i, 1] = e.dxf.vtx0[1]
-    pointArray[i, 2] = e.dxf.vtx0[2]
-
-    pointArray[i+1, 0] = e.dxf.vtx1[0]
-    pointArray[i+1, 1] = e.dxf.vtx1[1]
-    pointArray[i+1, 2] = e.dxf.vtx1[2]
-
-    pointArray[i+2, 0] = e.dxf.vtx2[0]
-    pointArray[i+2, 1] = e.dxf.vtx2[1]
-    pointArray[i+2, 2] = e.dxf.vtx2[2]
-
-    cellArray[faceCounter, :] = [3, i, i+1, i+2]  # need the 3 at the start to tell pyvista its a triangulation
-    cellData.append(e.dxf.layer)
-    i = i+3
-    faceCounter = faceCounter+1
-
-cellArray = np.hstack(cellArray)
-surf = pv.PolyData(pointArray, cellArray)
-
-plotter = pv.Plotter()
-plotter.add_mesh(surf, show_edges=False)
-plotter.show()
--- miningpy/visualisation/dxf.py
+++ miningpy/visualisation/dxf.py
@@ -14,6 +14,7 @@ def plot3D_dxf(path:              str = None,
                show_plot:         bool = True) -> pv.Plotter:
     """
     create activate 3D vtk plot of dxf that is fully interactive
+    the dxf has to either be strings or a triangulation
 
     Parameters
     ----------
@@ -41,7 +42,101 @@ def plot3D_dxf(path:              str = None,
 
     # get dxf data
     msp = doc.modelspace()
+
+    # check whether the dxf is strings or a triangulation
+    # check whether there are 3D faces or polylines and then
+    # call the correct utility function
     faces = msp.query('3DFACE')
+    strings = msp.query('POLYLINE')
+
+    # call correct utility function
+    if len(faces) > 0 and len(strings) == 0:
+        mesh = __plot_triangles(faces)
+
+    elif len(faces) == 0 and len(strings) > 0:
+        mesh = __plot_strings(strings)
+
+    else:
+        raise Exception("MiningPy ERROR - DXF not recognised")
+
+    # set theme
+    pv.set_plot_theme("ParaView")  # just changes colour scheme
+
+    # create polydata pyvista object
+    # plot dxf in pyvista window
+    p = pv.Plotter(notebook=False, title="MiningPy DXF Viewer")
+
+    if not cross_section:
+        p.add_mesh(mesh=mesh,
+                   show_edges=show_wireframe,
+                   color=colour)
+
+    if cross_section:
+        p.add_mesh_clip_plane(mesh=mesh,
+                              show_edges=show_wireframe,
+                              color=colour)
+
+    if show_grid:
+        p.show_grid()
+
+    p.show_axes()
+
+    if show_plot:
+        p.show(full_screen=True)
+
+    return p
+
+
+def __plot_strings(strings):
+    """
+    Utility function to produce a PyVista surface that can
+    then be plotted in a window for the user
+
+    Used for DXF of strings (i.e. pit design)
+
+    Returns a pv.PolyData object
+    """
+
+    # extract points and faces from dxf object
+    numLines = len(strings)
+    pointArray = np.zeros((numLines * 2, 2))
+    lineArray = np.zeros((numLines, 3), dtype='int')
+    cellData = []
+
+    i = 0
+    lineCounter = 0
+    name = []
+    for e in strings:
+        pointArray[i, 0] = e.dxf.vtx0[0]
+        pointArray[i, 1] = e.dxf.vtx0[1]
+        pointArray[i, 2] = e.dxf.vtx0[2]
+
+        pointArray[i + 1, 0] = e.dxf.vtx1[0]
+        pointArray[i + 1, 1] = e.dxf.vtx1[1]
+        pointArray[i + 1, 2] = e.dxf.vtx1[2]
+
+        pointArray[i + 2, 0] = e.dxf.vtx2[0]
+        pointArray[i + 2, 1] = e.dxf.vtx2[1]
+        pointArray[i + 2, 2] = e.dxf.vtx2[2]
+
+        cellArray[faceCounter, :] = [3, i, i + 1, i + 2]  # need the 3 at the start to tell pyvista its a triangulation
+        cellData.append(e.dxf.layer)
+        i = i + 3
+        faceCounter = faceCounter + 1
+
+    cellArray = np.hstack(cellArray)
+    return pv.PolyData(pointArray, cellArray)
+
+
+def __plot_triangles(faces):
+    """
+    Utility function to produce a PyVista surface that can
+    then be plotted in a window for the user
+
+    Used for DXF triangulations
+
+    Returns a pv.PolyData object
+    """
 
     # extract points and faces from dxf object
     num3Dfaces = len(faces)
@@ -71,34 +166,7 @@ def plot3D_dxf(path:              str = None,
         faceCounter = faceCounter + 1
 
     cellArray = np.hstack(cellArray)
-    surf = pv.PolyData(pointArray, cellArray)
-
-    # set theme
-    pv.set_plot_theme("ParaView")  # just changes colour scheme
-
-    # create polydata pyvista object
-    # plot dxf in pyvista window
-    p = pv.Plotter(notebook=False, title="MiningPy DXF Viewer")
-
-    if not cross_section:
-        p.add_mesh(mesh=surf,
-                   show_edges=show_wireframe,
-                   color=colour)
-
-    if cross_section:
-        p.add_mesh_clip_plane(mesh=surf,
-                              show_edges=show_wireframe,
-                              color=colour)
-
-    if show_grid:
-        p.show_grid()
-
-    p.show_axes()
-
-    if show_plot:
-        p.show(full_screen=True)
-
-    return p
+    return pv.PolyData(pointArray, cellArray)
 
 
 def face_position_dxf():
