09f66b826e28a04e5ecdd1cf9e4289d0224101bb
IainFullelove <fullelove.iain@gmail.com>
2020-08-26 18:46:34 +1000

    testing vtk dependency

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ install_requires = ['ezdxf>=0.13.1',
                     'numpy',
                     'pandas',
                     'pyvista',
-                    'vtk>=8.0',
+                    'vtk>=8.0,<9.0',
                     'matplotlib'
                     ]
 
