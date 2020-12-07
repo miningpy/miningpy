cccaa564185fa3dec35e1c5d60ce6450052b8816
IainFullelove <fullelove.iain@gmail.com>
2020-12-07 10:44:53 +1000

    resolved #65 no issues doing plot3D with only 1 block - added test to check

--- tests/test_plot3D.py
+++ tests/test_plot3D.py
@@ -5,7 +5,7 @@ import miningpy
 import pyvista as pv
 
 # test data
-testdata = {
+testdata1 = {
     'x': [5, 5, 15],
     'y': [5, 15, 25],
     'z': [5, 5, 5],
@@ -16,7 +16,29 @@ testdata = {
 def test_plot3D_1():
     # test blocks2vtk
     # all params specified
-    data = pd.DataFrame(testdata)
+    data = pd.DataFrame(testdata1)
+    plot = data.plot3D(
+        xyz_cols=('x', 'y', 'z'),
+        dims=(5, 5, 5),
+        col='ton',
+        show_plot=False
+    )
+
+    assert isinstance(plot, pv.Plotter)
+
+
+# test data - only one block
+testdata2 = {
+    'x': [5],
+    'y': [5],
+    'z': [5],
+    'ton': [50],
+}
+
+
+def test_plot3D_2():
+    # test blocks2vtk
+    data = pd.DataFrame(testdata2)
     plot = data.plot3D(
         xyz_cols=('x', 'y', 'z'),
         dims=(5, 5, 5),
