af67a733a8fb19175ea21a118c7cc7c3e2df0226
rohin <rshah@imcm.com.au>
2022-03-29 17:02:48 +1000

    annotations and False plot toggle

--- tests/test_grade_tonnage_plot.py
+++ tests/test_grade_tonnage_plot.py
@@ -22,8 +22,9 @@ cog_grades = [2.5, 12.5, 50]
 # write test
 def test_grade_tonnage_1():
     data = pd.DataFrame(testdata1)
-    output = data.grade_tonnage_plot(grade_col='cu', ton_col='tonnage', cog_grades=cog_grades, show_plot=True)
+    output = data.grade_tonnage_plot(grade_col='cu', ton_col='tonnage', cog_grades=cog_grades, show_plot=False)
     result = pd.DataFrame(resultdata1)
 
+    # check result
     check = (output.sum() - result.sum()).sum()
     assert check.sum() < 0.0001, "grade tonnage table not the same"
