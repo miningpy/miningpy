59d5d07b73986682e368323a6a970f03beeba635
IainFullelove <fullelove.iain@gmail.com>
2022-03-29 14:40:33 +1000

    grade tonnage plot test in function

--- tests/test_grade_tonnage_plot.py
+++ tests/test_grade_tonnage_plot.py
@@ -2,10 +2,10 @@
 import pandas as pd
 import miningpy
 
-# test data
-url = "https://drive.google.com/uc?export=download&id=1RWuGs5Sf54FLPyDM1K-fK0a7Im9apW0t"
-data = pd.read_csv(url, compression='zip')
 
-data.grade_tonnage_plot(grade_col='cu', ton_col='tonn', table_path='grade_tonnage_plot.xlsx',
-                        plot_path='grade_t.png', show_plot=True)
+def test_grade_tonnage_plot_1():
+    url = "https://drive.google.com/uc?export=download&id=1RWuGs5Sf54FLPyDM1K-fK0a7Im9apW0t"
+    data = pd.read_csv(url, compression='zip')
+    data.grade_tonnage_plot(grade_col='cu', ton_col='tonn', table_path='grade_tonnage_plot.xlsx',
+                            plot_path='grade_t.png', show_plot=True)
 
